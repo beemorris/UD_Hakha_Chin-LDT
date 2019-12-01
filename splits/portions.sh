858f60d0a1be7ba6167e8f0f189fad5e5e08f31d
beemorris <morrisrebeccar@gmail.com>
2019-11-30 22:57:19 -0500

    400 sentences added --tag

--- splits/portions.sh
+++ splits/portions.sh
@@ -8,7 +8,7 @@ for test in *.conllu; do
 	cp $test $test".test"
 	echo "$test $train"
 	/Users/bee/udpipe/src/udpipe --train --tokenizer none $test".train.udpipe" < $test".train"
-	/Users/bee/udpipe/src/udpipe --parse $test".train.udpipe" $test".test" > $test".output"
+	/Users/bee/udpipe/src/udpipe --tag --parse $test".train.udpipe" $test".test" > $test".output"
 	python ../conll18_ud_eval.py -v $test".test" $test".output"
 	#python ../conll18_ud_eval $test".test $test".output
 done

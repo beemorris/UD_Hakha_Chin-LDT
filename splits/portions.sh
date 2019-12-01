cf46ef027a8c6bcfca8db657bb2a0a68fdf90fa8
beemorris <morrisrebeccar@gmail.com>
2019-11-30 20:08:51 -0500

    400 sentences

--- splits/portions.sh
+++ splits/portions.sh
@@ -1,13 +1,14 @@
 for test in *.conllu; do
 	train=$(ls -1 *.conllu | grep -v $test | tr '\n' ' ');
-	echo $train
-	cat $train | ggrep -v '^#[^=]\+$' | python ../nodouble.py > $test".train"
-	python ~/tools/validate.py --lang cnh $test".train"
-	exit;
+	cat $train > $test".train"
+	#echo $train
+	#cat $train | ggrep -v '^#[^=]\+$' | python ../nodouble.py > $test".train"
+	#python ~/tools/validate.py --lang cnh $test".train"
+	#exit;
 	cp $test $test".test"
 	echo "$test $train"
 	/Users/bee/udpipe/src/udpipe --train --tokenizer none $test".train.udpipe" < $test".train"
-	/Users/bee/udpipe/src/udpipe --parse $test".test" > $test".output"
-	#python ../cconll18_ud_eval > $test".test" $test".output"
+	/Users/bee/udpipe/src/udpipe --parse $test".train.udpipe" $test".test" > $test".output"
+	python ../conll18_ud_eval.py -v $test".test" $test".output"
 	#python ../conll18_ud_eval $test".test $test".output
 done
